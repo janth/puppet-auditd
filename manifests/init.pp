86c2abadd7b4948a022abbf00d330f62952d1bca
James Kelly <james@hicag.org>
2019-01-04 13:27:57 -0600

    Delint

--- manifests/init.pp
+++ manifests/init.pp
@@ -508,8 +508,9 @@ class auditd (
     content => template('auditd/audispd.conf.erb'),
   }
 
-  # If a hash of rules is supplied with class then call auditd::rules defined type to apply them
-  $rules.each |$key,$opts| { 
+  # If a hash of rules is supplied with class then call auditd::rules defined
+  # type to apply them
+  $rules.each |$key,$opts| {
     auditd::rule { $key:
       * => pick($opts,{}),
     }
